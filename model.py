832cf36d14f50b68fcaf9fdf2365c06279b9fe53
rajabhupati.k.s <rajabhupati.k.s@gmail.com>
2024-12-31 17:16:43 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,19 @@
+import spacy
+from spacy.cli import download
+import os
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
+
+
+
+
+
+
+
+
+
